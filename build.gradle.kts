4e1ebd39f50c57818e38532d71f801bcc49aca95
Sai Shashaank V V <saishashaankvv@gmail.com>
2024-06-26 04:06:41 +0100

    build.yml fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,9 +53,7 @@ tasks.withType<Test> {
 
 tasks.withType<JacocoReport> {
     reports {
-        xml.required.set(false)
-        csv.required.set(false)
-        html.required.set(true)
+        csv.required.set(true)
     }
 
     afterEvaluate {
